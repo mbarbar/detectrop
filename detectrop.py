2429012faf715cec706138e36e92b6eb92118e83
Mohamad Barbar <mbarbar@runbox.com>
2017-12-14 11:23:50 +1100

    Clean up code into functions.

--- detectrop.py
+++ detectrop.py
@@ -7,6 +7,23 @@ import sys
 import subprocess
 import struct
 
+def write_gadgets(gadget_file):
+    with open(gadget_file, 'w') as gf:
+        subprocess.call(("ROPgadget --depth 4 --all --binary "
+                         + binary).split(' '), stdout=gf)
+
+
+def populate_gadget_addresses(gadgets_dict, gadget_file):
+    pattern_addr = re.compile("^0x[0-9a-f]+")
+    with open(gadget_file, 'r') as gf:
+        for line in gf:
+            # Match the address from ROPgadget's output.
+            addr = re.findall(pattern_addr, line)
+            if len(addr) == 0:
+                continue
+
+            # Add that address as a gadget - True is a placeholder.
+            gadgets_dict[struct.pack("L", int(addr[0], 16))] = True
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -20,23 +37,11 @@ if __name__ == "__main__":
 
     print("{}: core dump - {} ; binary - {}".format(exe, coredump, binary))
 
-    gadgets = {}
-
     gadget_file = "gadget_file"  # TODO: unique name?
+    write_gadgets(gadget_file)
 
-    with open(gadget_file, 'w') as gf:
-        subprocess.call(("ROPgadget --depth 4 --all --binary "
-                         + binary).split(' '), stdout=gf)
-
-    pattern_addr = re.compile("^0x[0-9a-f]+")
-    with open(gadget_file, 'r') as gf:
-        for line in gf:
-            # Match the address from ROPgadget's output.
-            addr = re.findall(pattern_addr, line)
-            if len(addr) == 0:
-                continue
-
-            # Add that address as a gadget - True is a placeholder.
-            gadgets[struct.pack("L", int(addr[0], 16))] = True
+    gadgets = {}
+    populate_gadget_addresses(gadgets, gadget_file)
 
     print(gadgets)
+
